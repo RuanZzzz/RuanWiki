0fdd722b6638cd7cda208d968159e70fc365ae5a
RuanZzzz <734162396@qq.com>
2021-12-15 15:53:36 +0800

    测试调通接口

--- src/main/java/com/richard/wiki/config/WikiApplication.java
+++ src/main/java/com/richard/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.richard.wiki;
+package com.richard.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.richard")
 @SpringBootApplication
 public class WikiApplication {
 

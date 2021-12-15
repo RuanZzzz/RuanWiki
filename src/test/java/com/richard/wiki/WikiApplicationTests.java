16ade19187cea4dbe3250a2c3c6d128adb096d26
RuanZzzz <734162396@qq.com>
2021-12-15 11:17:58 +0800

    初始化项目

---
+++ src/test/java/com/richard/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

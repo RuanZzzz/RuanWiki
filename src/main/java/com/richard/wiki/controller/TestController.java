afceaed461e5e21e76ee2b7fc3fb8876cda952e2
RuanZzzz <734162396@qq.com>
2021-12-16 23:03:28 +0800

    使用http client进行接口测试get请求和post请求

--- src/main/java/com/richard/wiki/controller/TestController.java
+++ src/main/java/com/richard/wiki/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.richard.wiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +12,11 @@ public class TestController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String hello() {
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World！Post，" + name;
     }
 
 }

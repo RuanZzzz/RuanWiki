47daae0a46c5806ef91785f99774f293c45899bd
RuanZzzz <734162396@qq.com>
2021-12-19 15:58:43 +0800

    新增ebook表，同时生成相关代码

---
+++ src/main/java/com/richard/wiki/domain/Ebook.java
@@ -0,0 +1,112 @@
+package com.richard.wiki.domain;
+
+public class Ebook {
+    private Long id;
+
+    private String name;
+
+    private Long categoryId1;
+
+    private Long categoryId2;
+
+    private String description;
+
+    private String cover;
+
+    private Integer docCount;
+
+    private Integer viewCount;
+
+    private Integer voteCount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCategoryId1() {
+        return categoryId1;
+    }
+
+    public void setCategoryId1(Long categoryId1) {
+        this.categoryId1 = categoryId1;
+    }
+
+    public Long getCategoryId2() {
+        return categoryId2;
+    }
+
+    public void setCategoryId2(Long categoryId2) {
+        this.categoryId2 = categoryId2;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Integer getDocCount() {
+        return docCount;
+    }
+
+    public void setDocCount(Integer docCount) {
+        this.docCount = docCount;
+    }
+
+    public Integer getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(Integer viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", categoryId1=").append(categoryId1);
+        sb.append(", categoryId2=").append(categoryId2);
+        sb.append(", description=").append(description);
+        sb.append(", cover=").append(cover);
+        sb.append(", docCount=").append(docCount);
+        sb.append(", viewCount=").append(viewCount);
+        sb.append(", voteCount=").append(voteCount);
+        sb.append("]");
+        return sb.toString();
+    }
+}

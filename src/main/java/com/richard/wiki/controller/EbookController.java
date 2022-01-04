c5ea137500d51f6d2604baf0384d65a9d0e45c2e
RuanZzzz <734162396@qq.com>
2022-01-04 23:28:00 +0800

    集成Validation，为列表接口做参数校验

--- src/main/java/com/richard/wiki/controller/EbookController.java
+++ src/main/java/com/richard/wiki/controller/EbookController.java
@@ -9,6 +9,8 @@ import com.richard.wiki.service.EbookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/ebook")
 public class EbookController {
@@ -17,7 +19,7 @@ public class EbookController {
     private EbookService ebookService;
 
     @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public CommonResp list(EbookQueryReq ebookQueryReq) {
+    public CommonResp list(@Valid EbookQueryReq ebookQueryReq) {
         CommonResp<PageResp<EbookQueryResp>> resp = new CommonResp<>();
         PageResp<EbookQueryResp> list = ebookService.list(ebookQueryReq);
         resp.setContent(list);
--- src/main/java/com/richard/wiki/req/PageReq.java
+++ src/main/java/com/richard/wiki/req/PageReq.java
@@ -3,12 +3,19 @@ package com.richard.wiki.req;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
 @Data
 public class PageReq {
 
+    @NotNull(message = "页码不能为空")
     @ApiModelProperty(name = "页码")
     private Integer page;
 
+    @NotNull(message = "每页条数不能为空")
+    @Max(value = 100,message = "每页条数不能超过100")
     @ApiModelProperty(name = "每页条数")
     private Integer pageSize;
 

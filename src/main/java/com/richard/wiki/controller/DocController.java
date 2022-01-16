6b85757fff6a374f1a309db53589d580f4c3b5b7
RuanZzzz <734162396@qq.com>
2022-01-16 19:20:12 +0800

    完成文档的all方法显示

--- src/main/java/com/richard/wiki/controller/DocController.java
+++ src/main/java/com/richard/wiki/controller/DocController.java
@@ -1,9 +1,14 @@
 package com.richard.wiki.controller;
 
+import com.richard.wiki.resp.CommonResp;
+import com.richard.wiki.resp.DocQueryResp;
 import com.richard.wiki.service.DocService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/doc")
@@ -12,6 +17,17 @@ public class DocController {
     @Autowired
     private DocService docService;
 
+    /**
+     * 返回文档所有信息
+     * @return
+     */
+    @RequestMapping(value = "/all",method = RequestMethod.GET)
+    private CommonResp all() {
+        CommonResp<List<DocQueryResp>> resp = new CommonResp<>();
+        List<DocQueryResp> list = docService.all();
+        resp.setContent(list);
 
+        return resp;
+    }
 
 }
--- src/main/java/com/richard/wiki/service/DocService.java
+++ src/main/java/com/richard/wiki/service/DocService.java
@@ -1,7 +1,45 @@
 package com.richard.wiki.service;
 
+import com.richard.wiki.domain.Doc;
+import com.richard.wiki.domain.DocExample;
+import com.richard.wiki.mapper.DocMapper;
+import com.richard.wiki.resp.DocQueryResp;
+import com.richard.wiki.util.SnowFlake;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class DocService {
+
+    @Autowired
+    private DocMapper docMapper;
+
+    @Autowired
+    private SnowFlake snowFlake;
+
+    /**
+     * 返回所有文档级别
+     * @return
+     */
+    public List<DocQueryResp> all() {
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        List<DocQueryResp> list = new ArrayList<>();
+        for (Doc doc : docList) {
+            DocQueryResp docQueryResp = new DocQueryResp();
+            docQueryResp.setId(doc.getId());
+            docQueryResp.setName(doc.getName());
+            docQueryResp.setParent(doc.getParent());
+            docQueryResp.setSort(doc.getSort());
+            list.add(docQueryResp);
+        }
+
+        return list;
+    }
+
 }

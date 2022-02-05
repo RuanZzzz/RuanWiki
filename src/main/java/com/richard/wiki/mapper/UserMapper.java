a845ca6748ffa662805fd248e05821bf5aa7b32c
RuanZzzz <734162396@qq.com>
2022-02-05 15:52:15 +0800

    重新生成user表的实体类（之前因为数据库中还有系统user表，导致生成的类有问题）

--- src/main/java/com/richard/wiki/mapper/UserMapper.java
+++ src/main/java/com/richard/wiki/mapper/UserMapper.java
@@ -10,27 +10,21 @@ public interface UserMapper {
 
     int deleteByExample(UserExample example);
 
-    int deleteByPrimaryKey(String user);
+    int deleteByPrimaryKey(Long id);
 
     int insert(User record);
 
     int insertSelective(User record);
 
-    List<User> selectByExampleWithBLOBs(UserExample example);
-
     List<User> selectByExample(UserExample example);
 
-    User selectByPrimaryKey(String user);
+    User selectByPrimaryKey(Long id);
 
     int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
-
-    int updateByExampleWithBLOBs(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByExample(@Param("record") User record, @Param("example") UserExample example);
 
     int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKeyWithBLOBs(User record);
 
     int updateByPrimaryKey(User record);
 }

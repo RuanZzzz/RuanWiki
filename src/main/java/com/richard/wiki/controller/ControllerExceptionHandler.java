458f068bd70800c53a4c4fc2d961688f7764ee0f
RuanZzzz <734162396@qq.com>
2022-02-06 22:46:54 +0800

    增加校验用户名不能重复，增加自定义异常

--- src/main/java/com/richard/wiki/controller/ControllerExceptionHandler.java
+++ src/main/java/com/richard/wiki/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.richard.wiki.controller;
 
+import com.richard.wiki.exception.BusinessException;
 import com.richard.wiki.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +29,36 @@ public class ControllerExceptionHandler {
         LOG.warn("参数校验失败：{}", e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
         commonResp.setSuccess(false);
         commonResp.setMessage(e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return commonResp;
+    }
+
+    /**
+     * 检验异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(BusinessException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.warn("业务异常：{}", e.getCode().getDesc());
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getCode().getDesc());
+        return commonResp;
+    }
+
+    /**
+     * 检验异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public CommonResp validExceptionHandler(Exception e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("系统异常：",e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage("系统出现异常，请联系管理员");
         return commonResp;
     }
 
--- src/main/java/com/richard/wiki/service/UserService.java
+++ src/main/java/com/richard/wiki/service/UserService.java
@@ -4,6 +4,8 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.richard.wiki.domain.User;
 import com.richard.wiki.domain.UserExample;
+import com.richard.wiki.exception.BusinessException;
+import com.richard.wiki.exception.BusinessExceptionCode;
 import com.richard.wiki.mapper.UserMapper;
 import com.richard.wiki.req.UserQueryReq;
 import com.richard.wiki.req.UserSaveReq;
@@ -12,6 +14,7 @@ import com.richard.wiki.resp.UserQueryResp;
 import com.richard.wiki.util.SnowFlake;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
@@ -66,9 +69,14 @@ public class UserService {
      */
     public void save(UserSaveReq req) {
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 如果没有传id，则进行保存
-            User user = User.builder().id(snowFlake.nextId()).loginName(req.getLoginName()).name(req.getName()).password(req.getPassword()).build();
-            userMapper.insert(user);
+            if (ObjectUtils.isEmpty(checkByLoginName(req.getLoginName()))) {
+                // 如果没有传id，则进行保存
+                User user = User.builder().id(snowFlake.nextId()).loginName(req.getLoginName()).name(req.getName()).password(req.getPassword()).build();
+                userMapper.insert(user);
+            }else {
+                // 提示用户名已经存在
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
         }else {
             // 如果传了id，则进行更新
             User user = User.builder().id(req.getId()).name(req.getName()).loginName(req.getLoginName()).password(req.getPassword()).build();
@@ -78,6 +86,22 @@ public class UserService {
 
     public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    /**
+     * 判断新增的时候，登录名是否已经存在
+     * @param loginName 传入的登录名
+     * @return 如果存在返回对象，否则返回null
+     */
+    public User checkByLoginName(String loginName) {
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andLoginNameEqualTo(loginName);
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        }else {
+            return userList.get(0);
+        }
     }
 
 }

e1beb909af2b636c1b418f6757bf56a35cd473b6
RuanZzzz <734162396@qq.com>
2022-02-12 15:19:29 +0800

    完成登录校验

--- src/main/java/com/richard/wiki/req/UserLoginReq.java
+++ src/main/java/com/richard/wiki/req/UserLoginReq.java
@@ -3,6 +3,7 @@ package com.richard.wiki.req;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -10,11 +11,11 @@ import javax.validation.constraints.Pattern;
 public class UserLoginReq {
 
     @ApiModelProperty(value = "登录名")
-    @NotNull(message = "登录名不能为空")
+    @NotEmpty(message = "登录名不能为空")
     private String loginName;
 
     @ApiModelProperty(value = "密码")
-    @NotNull(message = "密码不能为空")
+    @NotEmpty(message = "密码不能为空")
     private String password;
 
 }

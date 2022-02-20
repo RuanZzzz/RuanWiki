0ea5b05d441de14acf449d39bab665df88aaf585
RuanZzzz <734162396@qq.com>
2022-02-20 12:19:50 +0800

    集成websocket

---
+++ src/main/java/com/richard/wiki/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.richard.wiki.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * 声明当前springboot应用要暴露、使用websocket
+ */
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}

ca64ffb7c0686dd9e2f267248cb51fea55042355
RuanZzzz <734162396@qq.com>
2022-02-24 20:02:39 +0800

    新增常量类，防止代码中写入魔鬼字符

---
+++ src/main/java/com/richard/wiki/common/CommonConstant.java
@@ -0,0 +1,15 @@
+package com.richard.wiki.common;
+
+/**
+ * 常量
+ */
+public class CommonConstant {
+
+    /**
+     * LOGIN_USER_INFO          用户信息存储的键名
+     * USER_TOKEN               请求头中存储token的键名
+     */
+    public static final String LOGIN_USER_INFO = "login_user_info";
+    public static final String USER_TOKEN = "token";
+
+}
